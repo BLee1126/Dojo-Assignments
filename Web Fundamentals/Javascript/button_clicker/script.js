d1dde0d0261357f62c2a3bfdea2285432477a2db
Brian Lee <blee1126@gmail.com>
2021-05-21 15:38:44 -0500

    button clicker

---
+++ Web Fundamentals/Javascript/button_clicker/script.js
@@ -0,0 +1,11 @@
+function nameSwitch(realName) {
+    console.log(`Is really ${realName}`)
+}
+
+function openGoogle(){
+    window.location.href = 'https://google.com';
+}
+
+function openSeattle() {
+    window.location.href = 'https://visitseattle.org/';
+}
