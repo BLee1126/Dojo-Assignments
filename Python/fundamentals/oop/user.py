dcd89b285a8912752a869250612b3b74c0dc0746
Brian Lee <blee1126@gmail.com>
2021-06-06 19:27:05 -0500

    chaining

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -6,36 +6,27 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print(f'User: {self.name}, Balance: {self.account_balance}')
+        return self
     def transfer_money(self, other, amount):
         self.account_balance -= amount
         other.account_balance += amount
+        return self
     
 brian = User('Brian Lee', 'blee1126@gmail.com')
 amy = User('Amy Christophersen', 'amy@gmail.com')
 brett = User('Brett Kim', 'bkim86@gmail.com')
 
-brian.make_deposit(100)
-brian.make_deposit(200)
-brian.make_deposit(300)
-brian.make_withdrawl(100)
-brian.display_user_balance()
+brian.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawl(100).display_user_balance()
 
-amy.make_deposit(200)
-amy.make_deposit(400)
-amy.make_withdrawl(50)
-amy.make_withdrawl(75)
-amy.display_user_balance()
+amy.make_deposit(200).make_deposit(400).make_withdrawl(50).make_withdrawl(75).display_user_balance()
 
-brett.make_deposit(700)
-brett.make_withdrawl(25)
-brett.make_withdrawl(300)
-brett.make_withdrawl(100)
-brett.display_user_balance()
+brett.make_deposit(700).make_withdrawl(25).make_withdrawl(300).make_withdrawl(100).display_user_balance()
 
-brian.transfer_money(amy, 300)
-brian.display_user_balance()
+brian.transfer_money(amy, 300).display_user_balance()
 amy.display_user_balance()
