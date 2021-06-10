11926803c48eb7535766a61740647581f940b508
Brian Lee <blee1126@gmail.com>
2021-06-09 20:24:18 -0500

    flask

---
+++ Python/flask/fundamentals/understanding_routing/hello.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/')   
+def hello_world():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<name>')
+def hi_name(name):
+    return("Hi " + str(name))
+
+@app.route('/repeat/<num>/<word>/')
+
+def repeat_hi(word, num):
+    return f'{str(word)}\n'*int(num)
+
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+# import statements, maybe some other routes
+# app.run(debug=True) should be the very last statement! 
+
+
+    app.run(debug=True)   
