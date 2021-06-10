db8f5fcafeda18a74dce188ef58541805a0e7675
Brian Lee <blee1126@gmail.com>
2021-06-10 13:44:56 -0500

    update playground

--- Python/flask/fundamentals/playground/playground.py
+++ Python/flask/fundamentals/playground/playground.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__) 
 @app.route('/')  
 def boxes1():
-    return render_template('index.html')
+    return render_template('index.html', times=3, color='lightskyblue')
 
-@app.route('/play/<int:num>') 
-def boxes2():
-    
+@app.route('/play/<int:x>') 
+def boxes2(x):
+    return render_template("index.html", times = x, color='lightskyblue' )
 
-
+@app.route('/play/<int:x>/<string:color>')
+def boxes3(x, color):
+    return render_template("index.html", times = x, color=color )
 
 if __name__=="__main__": 
     app.run(debug=True)   
