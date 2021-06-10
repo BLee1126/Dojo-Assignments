e91943c34fdbffc28175047653df9608beef6968
Brian Lee <blee1126@gmail.com>
2021-06-10 17:35:41 -0500

    update to checkerboard

--- Python/flask/fundamentals/checkerboard/main.py
+++ Python/flask/fundamentals/checkerboard/main.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__) 
 
-@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+
+@app.route('/')
 def something(x, y, color1, color2):
 
-    return render_template("index.html",x=x, y=y,color1=color1, color2=color2 ) 
+    return render_template("index.html",x=8, y=8,color1='red', color2='black' ) 
+
+@app.route('/<int:x>')
+def something(x, y, color1, color2):
+
+    return render_template("index.html",x=x, y=8,color1='red', color2='black' ) 
+
+
+@app.route('/<int:x>/<int:y>)
+def something(x, y, color1, color2):
+
+    return render_template("index.html",x=x, y=y,color1='red', color2='black' ) 
 
 @app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
 def something(x, y, color1, color2):
