926767fe366217d1cfdc9f538dd4bdd2a41709a0
Brian Lee <blee1126@gmail.com>
2021-06-11 16:28:29 -0500

    update dojo survey

--- Python/flask/fundamentals/dojo_survey/server.py
+++ Python/flask/fundamentals/dojo_survey/server.py
@@ -12,8 +12,21 @@ def results():
     tloc = request.form['location']
     tlang = request.form['language']
     tcom = request.form['comment']
+    tprog = request.form['program']
+    if 'pet1' in request.form:
+        tpet1 = request.form['pet1']
+    elif 'pet1' not in request.form:
+        tpet1 = ''
+    if 'pet2' in request.form:
+        tpet2 = request.form['pet2']
+    elif 'pet2' not in request.form:
+        tpet2 = ''    
+    if 'pet3' in request.form:
+        tpet3 = request.form['pet3']
+    elif 'pet3' not in request.form:
+        tpet3 = ''
 
-    return render_template("results.html", tname = tname, tloc = tloc, tlang = tlang, tcom=tcom)
+    return render_template("results.html", tname = tname, tloc = tloc, tlang = tlang, tcom=tcom, tprog = tprog, tpet1=tpet1, tpet2=tpet2, tpet3=tpet3)
 
 
 
