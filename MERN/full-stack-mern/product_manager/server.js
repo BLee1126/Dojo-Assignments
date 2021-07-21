8d347fc8dc06d99b30d77e1a007fbe9ad7a1f5f7
Brian Lee <blee1126@gmail.com>
2021-07-21 16:41:34 -0500

    product_manager III

--- MERN/full-stack-mern/product_manager/server.js
+++ MERN/full-stack-mern/product_manager/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8000;
+// database
 require('./server/config/mongoose.config'); 
 app.use(cors());
 app.use(express.json(), express.urlencoded({ extended: true }));
+// routes
 require('./server/routes/product_manager.routes.js')(app);
 app.listen(port, () => {
     console.log(`Listening at Port ${port}!`)
